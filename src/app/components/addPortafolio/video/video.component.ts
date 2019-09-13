87b6aba5c20a606298cb63890000977bc686eb75
Andres Quinto Quinto <andrquqn@suramericana.com.co>
2019-09-13 16:31:53 -0500

    Cambios

--- src/app/components/addPortafolio/video/video.component.ts
+++ src/app/components/addPortafolio/video/video.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoComponent implements OnInit {
 
+  mensaje = 'Video para aumentar like';
+  likes: any = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sumarLike(video): void {
+    document.getElementById('like').style.color = '#ff0000';
+    this.likes += 1;
+  }
+
+  comentar(): void {
+
+    // tslint:disable-next-line:prefer-const
+    let comentario = document.getElementById('txtComent') as HTMLInputElement;
+
+    if (comentario.value === '') {
+      alert('Debes escribir un comentario');
+    } else {
+      alert('Comentario guardado');
+    }
   }
 
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -14,7 +14,7 @@ export class HeaderComponent implements OnInit {
 
   scrollToElement($element): void {
     console.log($element);
-    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    $element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
   }
 
 }
