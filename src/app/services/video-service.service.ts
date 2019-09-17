3e0894b48a89d6b06ffd5e9df846fed083ff4ccd
Andres Quinto Quinto <andrquqn@suramericana.com.co>
2019-09-17 15:22:56 -0500

    Se agrega toast

--- src/app/services/video-service.service.ts
+++ src/app/services/video-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Video } from '../shared/models/video';
 import { Observable, throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 import {retry, catchError, tap} from 'rxjs/operators';
 
 @Injectable({
@@ -11,7 +12,7 @@ export class VideoServiceService {
 
   URL_API = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -20,31 +21,11 @@ export class VideoServiceService {
   };
 
   getVideos(): Observable<Video> {
-    return this.http.get<Video>(this.URL_API + '/videos', this.httpOptions)
-      .pipe(
-        retry(1),
-        catchError(this.handleError)
-      );
+    return this.http.get<Video>(this.URL_API + '/videos', this.httpOptions);
   }
 
   getVideoById(id: string): Observable<Video> {
-    return this.http.get<Video>(this.URL_API + '/videos/' + id).pipe(
-      retry(1),
-      catchError(this.handleError)
-    );
-  }
-
-  handleError(error) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      // Get client-side error
-      errorMessage = error.error.message;
-    } else {
-      // Get server-side error
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-    }
-    window.alert(errorMessage);
-    return throwError(errorMessage);
+    return this.http.get<Video>(this.URL_API + '/videos/' + id);
   }
 
 }
